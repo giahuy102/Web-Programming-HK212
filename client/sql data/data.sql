92194809c0a3435dc2a0364d65f9ff56b6790874
Nhan vo <nhanvohmd1619@gmail.com>
2022-06-11 12:54:40 +0700

    push data sql

---
+++ client/sql data/data.sql
@@ -0,0 +1,15 @@
+USE RESTAURANT_2022;
+
+INSERT INTO _user VALUES (1, 'member', 'Nhan Vo', 'password', 'nhanvohmd1@gmail.com', '0919074981', 'Kien Giang');
+INSERT INTO _user VALUES (2, 'member', 'Vo Thien', 'password', 'nhanvohmd2@gmail.com', '0919074982', 'Kien Giang');
+INSERT INTO _user VALUES (3, 'member', 'Nguyen Vo', 'password', 'nhanvohmd3@gmail.com', '0919074983', 'Kien Giang');
+INSERT INTO _user VALUES (4, 'member', 'Nguyen Vo 1', 'password', 'nhanvohmd4@gmail.com', '0919074984', 'Kien Giang');
+INSERT INTO _user VALUES (5, 'member', 'Nguyen Vo 2', 'password', 'nhanvohmd5@gmail.com', '0919074985', 'Kien Giang');
+INSERT INTO _user VALUES (6, 'member', 'Nguyen Vo 3', 'password', 'nhanvohmd6@gmail.com', '0919074986', 'Kien Giang');
+INSERT INTO _user VALUES (7, 'member', 'Nguyen Vo 4', 'password', 'nhanvohmd7@gmail.com', '0919074987', 'Kien Giang');
+INSERT INTO _user VALUES (8, 'member', 'Nguyen Vo 5', 'password', 'nhanvohmd8@gmail.com', '0919074988', 'Kien Giang');
+INSERT INTO _user VALUES (9, 'member', 'Nguyen Vo 6', 'password', 'nhanvohmd9@gmail.com', '0919074989', 'Kien Giang');
+INSERT INTO _user VALUES (10, 'member', 'Nguyen Vo 7', 'password', 'nhanvohmd10@gmail.com', '0919074923', 'Kien Giang');
+INSERT INTO _user VALUES (11, 'member', 'Nguyen Vo 8', 'password', 'nhanvohmd11@gmail.com', '0919074945', 'Kien Giang');
+INSERT INTO _user VALUES (12, 'member', 'Nguyen Vo 9', 'password', 'nhanvohmd12@gmail.com', '0919074967', 'Kien Giang');
+INSERT INTO _user VALUES (13, 'member', 'Nguyen Vo 10', 'password', 'nhanvohmd13@gmail.com', '0919074912', 'Kien Giang');
