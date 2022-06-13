d2fbb1eda7b9eb7c7a76f991953675a2304a959f
duytran-vio <ducduyvio@gmail.com>
2022-06-13 23:42:18 +0700

    add upload image in dashboard

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,8 @@ $router->get("/^\/home\/product\/[0-9]+\/comment$/", array($adminController, 'ge
 $router->post("/^\/home\/product\/[0-9]+\/comment$/", array($adminController, 'add_product_comment'));
 $router->get("/^\/home\/user\/[0-9]+$/", array($adminController, 'get_user_by_id'));
 $router->post("/^\/home\/user\/[0-9]+$/", array($adminController, 'update_user_by_id'));
+$router->post("/^\/dashboard\/uploadImg$/", array($adminController, 'upload_image_to_storage'));
+
 $router->run();
 
 
--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -289,4 +289,55 @@ class AdminController
         $id = (int)($split_uri[count($split_uri) - 1]);
         $result = $this->modelAdmin->delete_one_product($id);
     }
+
+    function upload_image_to_storage($request){
+        $response = array();
+        $upload_dir = 'uploads/image/';
+        $server_url = 'http://localhost';
+
+        if($_FILES['avatar'])
+        {
+            $avatar_name = $_FILES["avatar"]["name"];
+            $avatar_tmp_name = $_FILES["avatar"]["tmp_name"];
+            $error = $_FILES["avatar"]["error"];
+
+            if($error > 0){
+                $response = array(
+                    "status" => "error",
+                    "error" => true,
+                    "message" => "Error uploading the file!"
+                );
+            }else 
+            {
+                $random_name = rand(1000,1000000)."-".$avatar_name;
+                $upload_name = $upload_dir.strtolower($random_name);
+                $upload_name = preg_replace('/\s+/', '-', $upload_name);
+
+                if(move_uploaded_file($avatar_tmp_name , $upload_name)) {
+                    $response = array(
+                        "status" => "success",
+                        "error" => false,
+                        "message" => "File uploaded successfully",
+                        "url" => $server_url."/".$upload_name
+                    );
+                }else
+                {
+                    $response = array(
+                        "status" => "error",
+                        "error" => true,
+                        "message" => "Error uploading the file!"
+                    );
+                }
+            }    
+
+        }else{
+            $response = array(
+                "status" => "error",
+                "error" => true,
+                "message" => "No file was sent!"
+            );
+        }
+
+        echo json_encode($response);
+    }
 }
